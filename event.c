5b546752a647d661e016f5c41373e83390f49724
PongphunKaewpongpok <138851758+PongphunKaewpongpok@users.noreply.github.com>
2024-11-01 20:47:41 +0700

    Update event.c

--- event.c
+++ event.c
@@ -1,21 +1,25 @@
-#include "weather.h"
+#include "event.h"
 
 #include "raylib.h"
 #include "data.h"
 #include "declaration.h"
 #include "drag_screen.h"
-#include "stdbool.h"
+#include <stdbool.h>
 #include "basic_math.h"
+#include "branch.h"
+#include <stdio.h>
 
 const int EVENT_ID_NONE = 0;
 const int EVENT_ID_FLOOD = 1;
-const int EVENT_ID_HUMAN = 2;
 
 int current_event = EVENT_ID_NONE;
 int flood_left = 0;
 int human_left = 0;
 
+int waterTokenSave = 0;
+
 int next_event_time = 720;
+
 
 void nextEventTime() {
     if (next_event_time > 0) {
@@ -27,20 +31,26 @@ void nextEventTime() {
 }
 
 void nextEventRandom() {
-    current_event = randomNumber(0, 2);
+    current_event = randomNumber(0, 1);
     if (current_event == EVENT_ID_FLOOD) {
-        
-    } else if (current_event == EVENT_ID_HUMAN) {
-        
+        flood_left = randomNumber(100*branch_count, 150*branch_count);
+        waterTokenSave = waterToken;
+        waterToken += flood_left;
     }
 }
 
-// void updateEvent() {
-//     if (current_event == EVENT_ID_NONE) {
-//         return;
-//     } else if (current_event == EVENT_ID_FLOOD) {
-//         DrawRectangle(0, SCREENHEIGHT, SCREENWIDTH, SCREENHEIGHT+screen_pos.y);
-//     } else if (current_event == EVENT_ID_HUMAN) {
+void updateEvent() {
+    if (current_event == EVENT_ID_NONE) {
+        return;
+    } else if (current_event == EVENT_ID_FLOOD) {
+        DrawRectangle(0, 725-((waterToken-waterTokenSave)/5)+screen_pos.y, SCREENWIDTH, ((waterToken-waterTokenSave)/5), (Color){0, 0, 255, 150});
+    }
+    if (waterToken-waterTokenSave <= 0) {
+        flood_left = 0;
+        current_event = EVENT_ID_NONE;
+    }
+}
 
-//     }
-// }
+int getCurrentEvent() {
+    return current_event;
+}
