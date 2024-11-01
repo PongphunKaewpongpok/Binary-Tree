f555b4d4028119deb66607a42de689a8e53ec100
PongphunKaewpongpok <138851758+PongphunKaewpongpok@users.noreply.github.com>
2024-11-01 20:46:30 +0700

    Update declaration.c

--- declaration.c
+++ declaration.c
@@ -20,6 +20,9 @@ Texture2D TEXTURE_BUTTON_CANT_ADD_LEAVE;
 Texture2D TEXTURE_BUTTON_CANT_ADD_FLOWER;
 Texture2D TEXTURE_BUTTON_CANT_ADD_FRUIT;
 Texture2D TEXTURE_BUTTON_CANT_REMOVE_BRANCH;
+Texture2D TEXTURE_BUTTON_BACK_TO_TREE;
+Texture2D TEXTURE_BUTTON_YES_BUTTON;
+Texture2D TEXTURE_BUTTON_NO_BUTTON;
 
 Texture2D TEXTURE_BRANCH;
 Texture2D TEXTURE_BRANCH_SELECTED;
@@ -27,8 +30,9 @@ Texture2D TEXTURE_GROUND;
 Texture2D TEXTURE_CLOUD;
 Texture2D TEXTURE_RAINDROP;
 Texture2D TEXTURE_SNOWFLAKE;
+Texture2D TEXTURE_FLOWER;
 Texture2D TEXTURE_TOKEN_BACKGROUND;
-Texture2D TEXTURE_ICON_SETTING;
+Texture2D TEXTURE_WARNING_BACKGROUND;
 
 Texture2D TEXTURE_APPLE_TOKEN;
 Texture2D TEXTURE_WATER_TOKEN;
@@ -54,21 +58,26 @@ void initTexture2D() {
     TEXTURE_BUTTON_CANT_ADD_FLOWER      = LoadTexture("Resources/button/cant_add_flower.png");
     TEXTURE_BUTTON_CANT_ADD_FRUIT       = LoadTexture("Resources/button/cant_add_fruit.png");
     TEXTURE_BUTTON_CANT_REMOVE_BRANCH   = LoadTexture("Resources/button/cant_remove_button.png");
+    TEXTURE_BUTTON_BACK_TO_TREE         = LoadTexture("Resources/button/back_to_tree.png");
+    TEXTURE_BUTTON_YES_BUTTON           = LoadTexture("Resources/button/yes_button.png");
+    TEXTURE_BUTTON_NO_BUTTON            = LoadTexture("Resources/button/no_button.png");
+
     TEXTURE_BRANCH                      = LoadTexture("Resources/branch.png");
     TEXTURE_BRANCH_SELECTED             = LoadTexture("Resources/branch_selected.png");
     TEXTURE_GROUND                      = LoadTexture("Resources/ground.png");
     TEXTURE_CLOUD                       = LoadTexture("Resources/cloud.png");
     TEXTURE_RAINDROP                    = LoadTexture("Resources/raindrops.png");
     TEXTURE_SNOWFLAKE                   = LoadTexture("Resources/snowflakes.png");
+    TEXTURE_FLOWER                      = LoadTexture("Resources/icon/flower.png");
 
     TEXTURE_APPLE_TOKEN                 = LoadTexture("Resources/icon/apple_token.png");
     TEXTURE_WATER_TOKEN                 = LoadTexture("Resources/icon/water_token.png");
     TEXTURE_SUN_TOKEN                   = LoadTexture("Resources/icon/daytime.png");
 
-    TEXTURE_ICON_SETTING                = LoadTexture("Resources/button/setting_button.png");
     TEXTURE_ICON_DAYTIME                = TEXTURE_SUN_TOKEN;
     TEXTURE_ICON_NIGHTTIME              = LoadTexture("Resources/icon/nighttime.png");
     TEXTURE_TOKEN_BACKGROUND            = LoadTexture("Resources/gui/token_bg.png");
+    TEXTURE_WARNING_BACKGROUND          = LoadTexture("Resources/gui/warning_bg.png");
 }
 
 void unloadAllTexture2D() {
@@ -85,6 +94,9 @@ void unloadAllTexture2D() {
     UnloadTexture(TEXTURE_BUTTON_CANT_ADD_FLOWER);
     UnloadTexture(TEXTURE_BUTTON_CANT_ADD_FRUIT);
     UnloadTexture(TEXTURE_BUTTON_CANT_REMOVE_BRANCH);
+    UnloadTexture(TEXTURE_BUTTON_BACK_TO_TREE);
+    UnloadTexture(TEXTURE_BUTTON_YES_BUTTON);
+    UnloadTexture(TEXTURE_BUTTON_NO_BUTTON);
 
     UnloadTexture(TEXTURE_BRANCH);
     UnloadTexture(TEXTURE_BRANCH_SELECTED);
@@ -92,13 +104,14 @@ void unloadAllTexture2D() {
     UnloadTexture(TEXTURE_CLOUD);
     UnloadTexture(TEXTURE_RAINDROP);
     UnloadTexture(TEXTURE_SNOWFLAKE);
+    UnloadTexture(TEXTURE_FLOWER);
 
     UnloadTexture(TEXTURE_APPLE_TOKEN);
     UnloadTexture(TEXTURE_WATER_TOKEN);
     UnloadTexture(TEXTURE_SUN_TOKEN);
 
-    UnloadTexture(TEXTURE_ICON_SETTING);
     UnloadTexture(TEXTURE_ICON_DAYTIME);
     UnloadTexture(TEXTURE_ICON_NIGHTTIME);
     UnloadTexture(TEXTURE_TOKEN_BACKGROUND);
+    UnloadTexture(TEXTURE_WARNING_BACKGROUND);
 }
