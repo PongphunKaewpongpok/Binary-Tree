ab3c9513f1f743b3354154bb47edecf129cfa0ad
PongphunKaewpongpok <138851758+PongphunKaewpongpok@users.noreply.github.com>
2024-11-01 20:50:32 +0700

    Update sound.h

--- sound.h
+++ sound.h
@@ -8,6 +8,8 @@ extern Music BGM_GARDEN_OF_FROST;
 extern Music BGM_MIDNIGHT_GARDEN;
 extern Music BGM_WHISPERS_OF_THE_RAIN;
 
+extern Sound COLLECT_APPLE;
+
 void initMusic();
 void unloadAllMusic();
 void updateMusic();
