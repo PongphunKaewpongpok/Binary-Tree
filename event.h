adae145cc87615c2db0e6672343c919eba087889
PongphunKaewpongpok <138851758+PongphunKaewpongpok@users.noreply.github.com>
2024-11-01 20:47:57 +0700

    Update event.h

--- event.h
+++ event.h
@@ -1,4 +1,15 @@
 #ifndef EVENT_H
 #define EVENT_H
 
+extern const int EVENT_ID_NONE;
+extern const int EVENT_ID_FLOOD;
+
+extern int flood_left;
+extern int waterTokenSave;
+
+void nextEventTime();
+void nextEventRandom();
+void updateEvent();
+int getCurrentEvent();
+
 #endif
